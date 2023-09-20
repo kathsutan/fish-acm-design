4c826fda8e0bcf88ef113fce446dd3a8a0c39810
katherinesutandar <katsutan@csu.fullerton.edu>
2023-09-19 19:53:42 -0700

    big eyed fishy

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -3,11 +3,21 @@ function setup() {
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(5, 163, 255);
-  }
+  fill(226, 69, 0);
+  ellipse(200, 200, 100, 50);
+    fill(226, 69, 0);
+  beginShape();
+    vertex(120,189);
+    vertex(120,223);
+    vertex(160,200);
+    fill(226,69,0);
+  endShape(CLOSE);
 
-  ellipse(mouseX, mouseY, 80, 80);
+  fill(255,255,255);
+  ellipse(234,200,50,50);
+  fill(0,0,0);
+  ellipse(245,200,11,11);
+  
+
+    
 }
